874b679ce9cc1a39dfe0d132f5ce733a7cf81b5e
mlazzell17@gmail.com <MicheSi>
2020-05-13 12:45:49 -0700

    completed mvp on BSTNode

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,65 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
-
+        # check if empty - if empty put node at root
+        if self.value is None:
+            self.value = BSTNode(value)
+        # if < go left
+        # if >= got right
+        else:
+            # if new value less than value in tree, add to left
+            if value < self.value:
+                if self.left is None:
+                    self.left = BSTNode(value)
+                else:
+                    self.left.insert(value)
+            # if new value is greater than value, add to right
+            else:
+                if self.right is None:
+                    self.right = BSTNode(value)
+                else:
+                    self.right.insert(value)
+        
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # start at root (self) when searching
+        # compare target to self
+        if target == self.value:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
-
+        # check if empty
+        if self.value is None:
+            return
+        # if not empty, traverse through right side to get last value on right
+        else:
+            if self.right is not None:
+                return self.right.get_max()
+            return self.value
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # check if empty
+        if self.value is None:
+            return
+        # traverse through nodes to get value of each
+        if self.value is not None:
+            fn(self.value)
+            # check left side for values
+            if self.left is not None:
+                self.left.for_each(fn)
+                # check right side for values
+            if self.right is not None:
+                self.right.for_each(fn)
 
     # Part 2 -----------------------
 
--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -16,36 +16,62 @@ class Node:
     self.next_node = new_next
 
 class LinkedList:
-    def __init__(self):
-        # first node in the list 
-        self.head = None
+  def __init__(self):
+      # first node in the list 
+    self.head = None
+    self.tail = None
 
-    def add_to_end(self, value):
-        # regardless of if the list is empty or not, we need to wrap the value in a Node 
-        new_node = Node(value)
-        # what if the list is empty? 
-        if not self.head:
-            self.head = new_node
-        # what if the list isn't empty?
-        else:
-            # what node do we want to add the new node to? 
-            # the last node in the list 
-            # we can get to the last node in the list by traversing it 
-            current = self.head 
-            while current.get_next() is not None:
-                current = current.get_next()
-            # we're at the end of the linked list 
-            current.set_next(new_node)
+  def add_to_head(self, value):
+    new_node = Node(value)
 
-    def remove_from_head(self):
-        # what if the list is empty?
-        if not self.head:
-            return None
-        # what if it isn't empty?
-        else:
-            # we want to return the value at the current head 
-            value = self.head.get_value()
-            # remove the value at the head 
-            # update self.head 
-            self.head = self.head.get_next()
-            return value
+    if not self.head and not self.tail:
+      self.head = new_node
+      self.add_to_tail = new_node
+    else:
+      new_node.set_next(self.head)
+      self.head = new_node
+
+
+  def add_to_tail(self, value):
+    # regardless of if the list is empty or not, we need to wrap the value in a Node 
+    new_node = Node(value)
+    # what if the list is empty? 
+    if not self.head and not self.tail:
+      self.head = new_node
+      self.tail = new_node
+    # what if the list isn't empty?
+    else:
+      self.tail.set_next(new_node)
+      self.tail = new_node
+
+  def contains(self, value):
+    current = self.head
+
+    while current is not None:
+      if current.get_value() == value:
+        return True
+      current = current.get_next()
+      return False
+    
+  def remove_head(self):
+    # what if the list is empty?
+    if not self.head and not self.tail:
+      return
+    # what if it isn't empty?
+    else:
+      # we want to return the value at the current head 
+      value = self.head.get_value()
+      # remove the value at the head 
+      # update self.head 
+      self.head = self.head.get_next()
+      return value
+
+  def get_max(self):
+    current = self.head
+    max = self.head.get_value()
+
+    while current is not None:
+      if current.value > max:
+        max = current.value
+        current = current.get_next()
+    return max
